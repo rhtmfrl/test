09ba965bea24560fed03cad485fe8c60e436240a
rhtmfrl <128569785+rhtmfrl@users.noreply.github.com>
2023-05-16 06:24:57 +0000

    조건문 끝

---
+++ .ccls-cache/@home@runner@test/8.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+// main(){
+//   int x;
+//   printf("점수를 입력하세요");
+//   scanf("%d", &x);
+  
+//     if(x>=90)
+//     printf("A");
+//   else if(x>=80)
+//     printf("B");
+//   else if(x>=70)
+//     printf("C");
+//   else if(x>=60)
+//     printf("D");
+//   else if(x<60)
+//     printf("F");
+// }
